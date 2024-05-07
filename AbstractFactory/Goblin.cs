7beb975a640024469d214c2710e35a3b679c00d0
MisterRus01 <leonardo.ugresic@gmail.com>
2024-05-07 11:08:25 +0200

    Add project files.

---
+++ AbstractFactory/Goblin.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbstractFactory
+{
+    public abstract class Wizard
+    {
+        public abstract void DoMagic();
+    }
+    public class FireWizard : Wizard
+    {
+        public override void DoMagic()
+        {
+            Console.WriteLine("Do Fire Magic");
+        }
+    }
+
+    public class WaterWizard : Wizard
+    {
+        public override void DoMagic()
+        {
+            Console.WriteLine("Do Water Magic");
+        }
+    }
+    public abstract class Goblin
+    {
+        public abstract void DoDamage();
+    }
+    public class FireGoblin : Goblin
+    {
+        public override void DoDamage()
+        {
+            Console.WriteLine("Do Fire Damage");
+        }
+    }
+
+    public class WaterGoblin : Goblin
+    {
+        public override void DoDamage()
+        {
+            Console.WriteLine("Do Water Damage");
+        }
+    }
+    public abstract class CharacterFactory
+    {
+        public abstract Wizard CreateWizard();
+        public abstract Goblin CreateGoblin();
+    }
+    public class WaterCharacter : CharacterFactory
+    {
+        public override Goblin CreateGoblin()
+        {
+            return new WaterGoblin();
+        }
+        public override Wizard CreateWizard()
+        {
+            return new WaterWizard();
+        }
+    }
+    public class FireCharacter : CharacterFactory
+    {
+        public override Goblin CreateGoblin()
+        {
+            return new FireGoblin();
+        }
+
+        public override Wizard CreateWizard()
+        {
+            return new FireWizard();
+        }
+    }
+   public class GameManager
+    {
+        public void Play(CharacterFactory characterFactory)
+        {          
+            characterFactory.CreateWizard();
+            characterFactory.CreateGoblin();
+        }
+    }
+}
---
+++ AbstractFactory/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbstractFactory
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            GameManager gameManager = new GameManager();
+            CharacterFactory waterCharacter = new WaterCharacter();
+            gameManager.Play(waterCharacter);
+            waterCharacter.CreateWizard().DoMagic();
+            waterCharacter.CreateGoblin().DoDamage();
+            CharacterFactory fireCharacter = new FireCharacter();
+            gameManager.Play(fireCharacter);
+            fireCharacter.CreateWizard().DoMagic();
+            fireCharacter.CreateGoblin().DoDamage();
+        }
+    }
+}
